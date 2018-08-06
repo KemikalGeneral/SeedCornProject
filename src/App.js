a0cd6ffecdc31aaeff740a87ecc262116d57b267
Richard Denton <kemikalgeneral@gmail.com>
2018-08-06 10:32:57 +0100

    Created the functionality for a user to input the required app's ID parameter from the GP store, and have the application scrape its associated data and return an app object. The details of which are dumped output to the screen.

--- src/App.js
+++ src/App.js
@@ -3,21 +3,22 @@ import './App.css';
 
 export default class App extends Component {
     state = {
-        title: '',
-        review: ''
+        appObject: {}
     };
 
+    // When the component mounts, callApi() is called and returns the appObject
+    // The returned appObject is then assigned to the client's appObject under a promise
     componentDidMount() {
         this.callApi()
             .then(res => this.setState({
-                title: res.title,
-                review: res.review
+                appObject: res.appObject
             }))
             .catch(err => console.log('Error: ', err));
     }
 
+    // Called from component mounting, returns a JSON of the appObject
     callApi = async () => {
-        const response = await fetch('/gPlay');
+        const response = await fetch('/gplay');
         const body = await response.json();
 
         if (response.status !== 200) {
@@ -28,13 +29,31 @@ export default class App extends Component {
     };
 
     render() {
-        console.log("==================== app log ====================");
-        console.log("State: ", this.state);
+        console.log("==================== Render ====================");
         return (
             <div className="App">
 
-                <p>Title: {this.state.title}.</p>
-                <p>Review: {this.state.review}.</p>
+                {/*Form to submit the app name*/}
+                <form action="/" method="post">
+                    <input type="text" name="appToSearch" />
+                    <input type="submit" value="Search"/>
+                </form>
+
+                <p>title: {this.state.appObject.title}.</p>
+                <p>contentRatingDescription: {this.state.appObject.contentRatingDescription}.</p>
+                <p>description: {this.state.appObject.description}.</p>
+                <p>developer: {this.state.appObject.developer}.</p>
+                <p>familyGenre: {this.state.appObject.familyGenre}.</p>
+                <p>genre: {this.state.appObject.genre}.</p>
+                <p>headerImage: {this.state.appObject.headerImage}.</p>
+                <p>icon: {this.state.appObject.icon}.</p>
+                <p>installs: {this.state.appObject.installs}.</p>
+                <p>price: {this.state.appObject.price}.</p>
+                <p>priceText: {this.state.appObject.priceText}.</p>
+                <p>ratings: {this.state.appObject.ratings}.</p>
+                <p>released: {this.state.appObject.released}.</p>
+                <p>reviews: {this.state.appObject.reviews}.</p>
+                <p>score: {this.state.appObject.score}.</p>
 
             </div>
         );
