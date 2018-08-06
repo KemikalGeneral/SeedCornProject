114188a6672df91f2b502f55466e20ff47c74484
Richard Denton <kemikalgeneral@gmail.com>
2018-08-06 10:36:42 +0100

    Removed unused JSON file.

--- server.js
+++ server.js
@@ -1,20 +1,43 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT||5000;
+const port = process.env.PORT || 5000;
+const bodyParser = require('body-parser');
+const googlePlayApp = require('google-play-scraper');
 
-const gPlay = require('google-play-scraper');
-const appToScrape = 'com.mojang.minecraftpe';
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/gPlay', (req, res) => {
-    const gPlayResults = gPlay.app({appId: appToScrape});
+let appToScrape = '';
 
-    console.log('==================== server log ====================');
-    gPlayResults.then(console.log);
+/**
+ * Using the appToScrape variable as the appId,
+ * search the Google Play Store for the requested app and return as a Promise.
+ * Use a callback to get the data to pass back to the front end.
+ */
+app.get('/gplay', (req, res) => {
+    console.log('==================== /gPlay ====================');
+    const gPlayResults = googlePlayApp.app({appId: appToScrape, country: 'gb'});
 
-    res.send({
-        title: 'title',
-        review: 'review',
+    gPlayResults.then(function (result) {
+        res.send({
+            appObject: result
+        })
     });
 });
 
+/**
+ * Assign the search request to the appToScrape variable.
+ * Redirect to the main page for population.
+ */
+app.post('/', (req, res) => {
+    appToScrape = req.body.appToSearch;
+
+    res.redirect('/gplay');
+});
+
 app.listen(port, () => console.log(`Listening on port: ${port}`));
+
+
+
+
+// com.mojang.minecraftpe
