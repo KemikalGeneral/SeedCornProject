a83606813a0ead0e7713e31926280121c83d6b6e
kemikalgeneral <kemikalgeneral@gmail.com>
2018-08-18 00:31:59 +0100

    Create a Database Helper class which will contain the database calls as functions.

--- Server.js
+++ Server.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,15 +9,10 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-let appToScrape = 'com.mojang.minecraftpe';
+const DatabaseHelper = require('./src/Database/DatabaseHelper').DatabaseHelper;
+const dbHelper = new DatabaseHelper();
 
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database(':memory:', (err) => {
-    if (err) {
-        return console.error(err.message);
-    }
-    console.log('Database connected');
-});
+let appToScrape = 'com.mojang.minecraftpe';
 
 /**
  * Using the appToScrape variable as the appId,
@@ -24,6 +21,9 @@ const db = new sqlite3.Database(':memory:', (err) => {
  */
 app.get('/gplay', (req, res) => {
     console.log('==================== /gPlay ====================');
+    if (appToScrape.length < 1) {
+        appToScrape = 'com.mojang.minecraftpe';
+    }
     const gPlayResults = googlePlayApp.app({appId: appToScrape, country: 'gb'});
     const gpReviews = googlePlayApp.reviews({appId: appToScrape});
 
@@ -33,59 +33,14 @@ app.get('/gplay', (req, res) => {
             res.send({
                 appObject: data,
                 reviewsObject: reviews
-            })
-        })
-    });
-
-    //TODO split into helper functions
-    const db = new sqlite3.Database(':memory:', (err) => {
-        if (err) {
-            return console.error(err.message);
-        }
-        console.log('Database connected');
-    });
-
-    db.serialize(function () {
-        console.log('serialize!', db);
-
-        db.run("CREATE TABLE IF NOT EXISTS app (name TEXT)");
-
-        db.run("INSERT INTO app(name) VALUES(?), (?), (?)", ['1', '2', '3'], function(err) {
-            console.log('========== INSERT ==========');
-            if (err) {
-                return console.error(err.message);
-            }
-
-            console.log(`Row inserted: ${this.lastID}`);
-        });
-
-        db.each("SELECT * FROM app", (err, row) => {
-            console.log('========== EACH ==========');
-            if (err) {
-                throw err;
-            }
-
-            console.log(`Each row: ${row.name}`);
-        });
-
-        db.all("SELECT * FROM app", (err, rows) => {
-            console.log('========== ALL ==========');
-            console.log('All rows: ', rows);
-
-            rows.forEach((row) => {
-                console.log('ForEachRow: ', row.name);
             });
-        });
 
-        db.close((err) => {
-            console.log('========== CLOSE ==========');
-            if (err) {
-                return console.error(err.message);
-            }
-            console.log('Closing database');
+            dbHelper.insert();
+            dbHelper.each();
+            dbHelper.all();
+            // dbHelper.close();
         })
     });
-
 });
 
 /**
