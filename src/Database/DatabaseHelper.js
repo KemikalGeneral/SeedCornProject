e8fd910d7924a69f3a711c99afcfa91418f9670e
kemikalgeneral <kemikalgeneral@gmail.com>
2018-08-20 21:45:52 +0100

    Split out and moved the table creation scripts to the bottom of the file for better readability. Added all fields for app and review data (review data will need to be mapped from the incoming array).

--- src/Database/DatabaseHelper.js
+++ src/Database/DatabaseHelper.js
@@ -7,10 +7,8 @@ let db;
  * * const DatabaseHelper = require('./src/Database/DatabaseHelper').DatabaseHelper;
  * * const dbHelper = new DatabaseHelper();
  * * dbHelper.insert();
+ * Table creation statements are at the bottom of this file.
  */
-
-//TODO expand the functions to take parameters
-
 class DatabaseHelper {
     /**
      * Constructor - Upon instantiating a DatabaseHelper class, this creates the tables in the callback.
@@ -27,39 +25,49 @@ class DatabaseHelper {
             }
 
             // db.run("DROP TABLE if exists app");
-            db.run('create table IF NOT EXISTS app(app_id integer primary key, app_name varchar(100) not null)');
-            db.run("create table IF NOT EXISTS review(review_id integer primary key, review_text text not null, app_id integer not null, constraint FK_app_id foreign key (app_id) references app(app_id));");
+            db.run(createAppTable);
+            db.run(createReviewTable);
 
             console.log('Database connected');
         });
     };
 
     /**
-     * insertNewAppAndReview - takes and app name and the associated review and saves them to their respective tables.
+     * insertNewAppAndReview - takes the app and associated review data and saves them to their respective tables.
      * Used the row number of the inserted app (lastID) as the foreign key for the review's app_id.
      * @param appName
+     * @param appDeveloper
+     * @param appSize
+     * @param appVersion
      * @param reviewText
+     * @param reviewScore
+     * @param reviewDate
      */
-    insertNewAppAndReview(appName, reviewText) {
+    insertNewAppAndReview(appName, appDeveloper, appSize, appVersion,
+                          reviewText, reviewScore, reviewDate) {
         // this.open();
 
         // Save app details
-        db.run("INSERT INTO app(app_name) VALUES(?)", [appName], function (err) {
-            console.log('\n========== INSERT - insertNewAppAndReview - app name ==========');
-            if (err) {
-                return console.error(err.message);
-            }
-            console.log(`App saved: ${appName}`);
-
-            // Save review details
-            db.run("INSERT INTO review(review_text, app_id) VALUES(?, ?)", [reviewText, this.lastID], function (err) {
-                console.log('\n========== INSERT - insertNewAppAndReview - review text ==========');
+        db.run(`INSERT INTO app (app_name, app_developer, app_size, app_version)
+                VALUES (?, ?, ?, ?)`, [appName, appDeveloper, appSize, appVersion],
+            function (err) {
+                console.log('\n========== INSERT - insertNewAppAndReview - app name ==========');
                 if (err) {
                     return console.error(err.message);
                 }
-                console.log(`Review saved: ${reviewText}`);
+                console.log(`App saved: ${appName}`);
+
+                // Save review details
+                db.run(`INSERT INTO review (review_text, review_score, review_date, app_id)
+                        VALUES (?, ?, ?, ?)`, [reviewText, reviewScore, reviewDate, this.lastID],
+                    function (err) {
+                        console.log('\n========== INSERT - insertNewAppAndReview - review text ==========');
+                        if (err) {
+                            return console.error(err.message);
+                        }
+                        console.log(`Review saved: ${reviewText}`);
+                    });
             });
-        });
         // this.close();
     };
 
@@ -68,8 +76,8 @@ class DatabaseHelper {
      */
     findAll() {
         // this.open();
-        db.all("SELECT * FROM app join review on app.app_id = review.app_id", (err, rows) => {
-            console.log('\n========== ALL ==========');
+        db.all(`SELECT * FROM app join review on app.app_id = review.app_id`, (err, rows) => {
+            console.log('\n========== FindAll ==========');
             console.log('All rows: \n', rows);
 
             rows.forEach((row) => {
@@ -85,8 +93,8 @@ class DatabaseHelper {
      */
     findOne() {
         // this.open();
-        db.each("SELECT * FROM app join review on app.App_id = review.app_id where app.app_id = 1", (err, row) => {
-            console.log('\n========== EACH - app ==========');
+        db.each(`SELECT * FROM app join review on app.App_id = review.app_id where app.app_id = 1`, (err, row) => {
+            console.log('\n========== FindOne ==========');
             console.log('Row: ', row);
             if (err) {
                 throw err;
@@ -117,5 +125,23 @@ class DatabaseHelper {
         });
     };
 }
+
+const createAppTable = `create table IF NOT EXISTS app (
+  app_id        integer primary key,
+  app_name      varchar(100) not null,
+  app_developer varchar(100) not null,
+  app_size      integer      not null,
+  app_version   integer      not null
+)`;
+
+const createReviewTable = `create table IF NOT EXISTS review (
+  review_id        integer primary key,
+  review_text      text        not null,
+  review_score     varchar(50) not null,
+  review_date      date        not null,
+  date_of_scraping date        not null default (datetime('now', 'localtime')),
+  app_id           integer     not null,
+  constraint FK_app_id foreign key (app_id) references app (app_id)
+)`;
 
 module.exports = {DatabaseHelper: DatabaseHelper};
